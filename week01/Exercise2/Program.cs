ed8f6f4f202a0820d88410a8412a808d1dfa6b7d
Snow <ssnowsoft1@gmail.com>
2025-10-29 15:23:41 +0100

    The names Print out correctly

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter your Grade:");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        if userInput >= 90
+        {
+            Console.ReadLine("A")
+        }
+        else if userInput >= 80
+        {
+            Console.ReadLine("B")
+        }
+        else if userInput >=70 
+        {
+            Console.ReadLine("C")
+        }
+        else if userInput >=60
+        {
+            Console.ReadLine("D")
+        }
+        else
+        {
+            Console.ReadLine("F")
+        }
     }
 }
