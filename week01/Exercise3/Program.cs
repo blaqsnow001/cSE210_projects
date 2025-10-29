b5d22f1d1e00439a2e4cde61d838239688d66bf1
Snow <ssnowsoft1@gmail.com>
2025-10-29 21:35:14 +0100

    Finished excersise 3 without the Stretch Challange

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        int guessCount = 0;
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+
+        Console.WriteLine("What is your guess ?");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You Guessed it!!");
+        }
+
+        while (guess != magicNumber)
+        {
+         
+            Console.WriteLine("What is your guess ?");
+            guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it!!");
+            }
+        }
     }
 }
