54928d0eef04832050a467bcb76732ccd2d9d215
Snow <ssnowsoft1@gmail.com>
2025-10-29 16:37:12 +0100

    I finished this project including the Stretch Challenge

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,32 +1,72 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter your Grade:");
-        string userInput = Console.ReadLine();
-        int number = int.Parse(userInput);
+        Console.WriteLine("Enter your Grade");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
 
-        if userInput >= 90
+        string letter;
+        
+        if (grade >= 90)
         {
-            Console.ReadLine("A")
+            letter = "A";
         }
-        else if userInput >= 80
+        else if (grade >= 80)
         {
-            Console.ReadLine("B")
+            letter = "B";
         }
-        else if userInput >=70 
+        else if (grade >= 70)
         {
-            Console.ReadLine("C")
+            letter = "C";
         }
-        else if userInput >=60
+        else if (grade >= 60)
         {
-            Console.ReadLine("D")
+            letter = "D";
         }
         else
         {
-            Console.ReadLine("F")
+            letter = "F";
+        }
+
+        string sign = "";
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (grade >= 93)
+        {
+            sign = "";
+        }
+
+        if (grade < 60)
+        {
+            sign = "";
+        }
+        
+        Console.WriteLine($"You received a {letter}{sign}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time.");
         }
     }
 }
