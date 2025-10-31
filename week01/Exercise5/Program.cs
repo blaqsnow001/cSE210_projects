028739ca33fafcba3720b883a3473b0f87c87b15
Snow <ssnowsoft1@gmail.com>
2025-10-31 01:54:47 +0100

    Completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program");
+        }  
+        DisplayWelcome();
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        string userName = PromptUserName();
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favourite number? ");
+            return int.Parse(Console.ReadLine());
+        }
+         int userNumber = PromptUserNumber();
+
+         static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        int square = SquareNumber(userNumber);
+
+
+         static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"Mr {name}, the square of your number is {square}.");
+        } 
+        DisplayResult(userName, square);
+        }
+
     }
-}
