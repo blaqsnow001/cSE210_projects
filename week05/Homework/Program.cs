feb4a96285fbd11d47c188488fa1c59ab576fa92
Snow <ssnowsoft1@gmail.com>
2025-11-28 12:50:44 +0100

    Add Assignment, MathAssignment, and WritingAssignment classes with functionality for assignment management

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+         // Test base Assignment class
+        Assignment simpleAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+        Console.WriteLine();
+
+        // Test MathAssignment class
+        MathAssignment mathAssignment = new MathAssignment(
+            "Roberto Rodriguez", 
+            "Fractions", 
+            "Section 7.3", 
+            "Problems 8-19"
+        );
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test WritingAssignment class
+        WritingAssignment writingAssignment = new WritingAssignment(
+            "Mary Waters", 
+            "European History", 
+            "The Causes of World War II"
+        );
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
-}
+    }
