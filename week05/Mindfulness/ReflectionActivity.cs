dc72a10bd1c0aa66c9dfd2bd4e06ede91d24d0da
Snow <ssnowsoft1@gmail.com>
2025-12-04 02:19:29 +0100

    Finished (with sample solution)

--- week05/Mindfulness/ReflectionActivity.cs
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -8,7 +8,7 @@ public class ReflectionActivity : Activity
 
     public ReflectionActivity() 
         : base("Reflection Activity", 
-               "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+            "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
     {
         _prompts = new List<string>
         {
